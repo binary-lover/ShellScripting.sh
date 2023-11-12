11b02563c1cd762a53c5b7aede70d5c5c5e2a437
binarylover <binarylover@nohat>
2023-11-12 19:14:13 +0530

    loops consept added

---
+++ loops.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+
+# for i in $(pwd) $(ls) # both starting and ending value is incluted
+# do
+#     echo $i 
+# done
+
+# handling files
+
+# File="/home/binarylover/Downloads/Notes/bashScripting.sh/file.txt"
+
+# for name in $($File)
+# do 
+#     echo "$name"
+# done
+
+
+# while loop
+
+# count=0
+# while [[ $count -lt 100 ]]
+# do
+#     echo "number is $count"
+#     (( count++ ))
+# done
+
+# until loop --Just opposite of while loop
+
+# a=10
+
+# until [ $a -ge 20 ] 
+# do
+#     echo "$a"
+#     (( a++ ))
+# done
+
+# echo "$(ls)"
+
+
+# infinite loop
+while true
+do
+    echo "hey bro!"
+    sleep 0.2s
+
+done
