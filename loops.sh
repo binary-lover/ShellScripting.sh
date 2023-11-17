364b01b924bbc5b3b0ac339bdbdaebb03497cf8f
Binary Lover <95335243+binary-lover@users.noreply.github.com>
2023-11-17 11:02:14 +0530

    Update loops.sh

--- loops.sh
+++ loops.sh
@@ -1,52 +1,52 @@
-#!/bin/bash
+# !/bin/bash
 
 
-# for i in $(pwd) $(ls) # both starting and ending value is incluted
-# do
-#     echo $i 
-# done
+for i in $(pwd) $(ls) # both starting and ending value is incluted
+do
+    echo $i 
+done
 
 # handling files
 
-# File="/home/binarylover/Downloads/Notes/bashScripting.sh/file.txt"
+File="/home/binarylover/Downloads/Notes/bashScripting.sh/file.txt"
 
-# for name in $($File)
-# do 
-#     echo "$name"
-# done
+for name in $($File)
+do 
+    echo "$name"
+done
 
 
-# while loop
+while loop
 
-# count=0
-# while [[ $count -lt 100 ]]
-# do
-#     echo "number is $count"
-#     (( count++ ))
-# done
+count=0
+while [[ $count -lt 100 ]]
+do
+    echo "number is $count"
+    (( count++ ))
+done
 
-# until loop --Just opposite of while loop
+until loop --Just opposite of while loop
 
-# a=10
+a=10
 
-# until [ $a -ge 20 ] 
-# do
-#     echo "$a"
-#     (( a++ ))
-# done
+until [ $a -ge 20 ] 
+do
+    echo "$a"
+    (( a++ ))
+done
 
-# echo "$(ls)"
+echo "$(ls)"
 
 
 # infinite loop
-# while true
-# do
-#     echo "hey bro!"
-#     sleep 0.2s
-# done
+while true
+do
+    echo "hey bro!"
+    sleep 0.2s
+done
 
 # Read files
-# while read myvar
-# do
-#     echo "Value in my file is $($myvar)"
-# done < file.txt
+while read myvar
+do
+    echo "Value in my file is $($myvar)"
+done < file.txt
